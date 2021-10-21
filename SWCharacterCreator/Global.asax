63511468178f067822eba8bec1f8a7554d5de257
Faith Sekerak <59906879+fmsekerak@users.noreply.github.com>
2021-10-20 23:43:01 -0400

    Add project files.

---
+++ SWCharacterCreator/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SWCharacterCreator.Global" Language="C#" %>
---
+++ SWCharacterCreator/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SWCharacterCreator.Startup))]
+namespace SWCharacterCreator
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
