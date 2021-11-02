62a3bd597b798651ae757d94c4191471c6d8e7c5
Faith Sekerak <59906879+fmsekerak@users.noreply.github.com>
2021-11-01 23:13:42 -0400

    Display Table

--- SWCharacterCreator/CharacterView.aspx
+++ SWCharacterCreator/CharacterView.aspx
@@ -11,4 +11,79 @@
   <asp:Button OnClick="displayCharacter" ID="displayCharacterButton" runat="server" Text="Display">
     </asp:Button> 
 
+    <br /> <br />
+
+   <asp:Table ID="DisplayCharacterTable" runat="server" BorderColor="SandyBrown" BorderWidth="2" Height="30" Width="1000">
+       <asp:TableRow ID="Row1" runat="server" BorderColor="SandyBrown">
+           <asp:TableCell ID="R1C1" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               Name
+           </asp:TableCell>
+           <asp:TableCell ID="R1C2" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               Species
+           </asp:TableCell>
+           <asp:TableCell ID="R1C3" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               Class
+           </asp:TableCell>
+           <asp:TableCell ID="R1C4" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               Level
+           </asp:TableCell>
+           <asp:TableCell ID="R1C5" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               Alignment
+           </asp:TableCell>
+           <asp:TableCell ID="R1C6" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               Strength
+           </asp:TableCell>
+           <asp:TableCell ID="R1C7" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               Dexterity
+           </asp:TableCell>
+           <asp:TableCell ID="R1C8" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               Constitution
+           </asp:TableCell>
+           <asp:TableCell ID="R1C9" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               Intelligence
+           </asp:TableCell>
+           <asp:TableCell ID="R1C10" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               Wisdom
+           </asp:TableCell>
+           <asp:TableCell ID="R1C11" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               Charisma
+           </asp:TableCell>
+       </asp:TableRow>
+       <asp:TableRow ID="DisplayRow" runat="server" BorderColor="SandyBrown">
+           <asp:TableCell ID="DisplayName" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               -
+           </asp:TableCell>
+           <asp:TableCell ID="DisplaySpecies" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               -
+           </asp:TableCell>
+           <asp:TableCell ID="DisplayClass" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               -
+           </asp:TableCell>
+           <asp:TableCell ID="DisplayLevel" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               -
+           </asp:TableCell>
+           <asp:TableCell ID="DisplayAlignment" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               -
+           </asp:TableCell>
+           <asp:TableCell ID="DisplayStr" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               -
+           </asp:TableCell>
+           <asp:TableCell ID="DisplayDex" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               -
+           </asp:TableCell>
+           <asp:TableCell ID="DisplayCon" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               -
+           </asp:TableCell>
+           <asp:TableCell ID="DisplayInt" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               -
+           </asp:TableCell>
+           <asp:TableCell ID="DisplayWis" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               -
+           </asp:TableCell>
+           <asp:TableCell ID="DisplayChar" BorderColor="SandyBrown" BorderWidth="2" runat="server" HorizontalAlign="Center">
+               -
+           </asp:TableCell>
+       </asp:TableRow>
+   </asp:Table>
+
 </asp:Content>
--- SWCharacterCreator/CharacterView.aspx.designer.cs
+++ SWCharacterCreator/CharacterView.aspx.designer.cs
@@ -31,5 +31,230 @@ namespace SWCharacterCreator
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button displayCharacterButton;
+
+        /// <summary>
+        /// DisplayCharacterTable control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table DisplayCharacterTable;
+
+        /// <summary>
+        /// Row1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableRow Row1;
+
+        /// <summary>
+        /// R1C1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell R1C1;
+
+        /// <summary>
+        /// R1C2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell R1C2;
+
+        /// <summary>
+        /// R1C3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell R1C3;
+
+        /// <summary>
+        /// R1C4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell R1C4;
+
+        /// <summary>
+        /// R1C5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell R1C5;
+
+        /// <summary>
+        /// R1C6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell R1C6;
+
+        /// <summary>
+        /// R1C7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell R1C7;
+
+        /// <summary>
+        /// R1C8 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell R1C8;
+
+        /// <summary>
+        /// R1C9 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell R1C9;
+
+        /// <summary>
+        /// R1C10 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell R1C10;
+
+        /// <summary>
+        /// R1C11 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell R1C11;
+
+        /// <summary>
+        /// DisplayRow control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableRow DisplayRow;
+
+        /// <summary>
+        /// DisplayName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell DisplayName;
+
+        /// <summary>
+        /// DisplaySpecies control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell DisplaySpecies;
+
+        /// <summary>
+        /// DisplayClass control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell DisplayClass;
+
+        /// <summary>
+        /// DisplayLevel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell DisplayLevel;
+
+        /// <summary>
+        /// DisplayAlignment control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell DisplayAlignment;
+
+        /// <summary>
+        /// DisplayStr control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell DisplayStr;
+
+        /// <summary>
+        /// DisplayDex control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell DisplayDex;
+
+        /// <summary>
+        /// DisplayCon control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell DisplayCon;
+
+        /// <summary>
+        /// DisplayInt control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell DisplayInt;
+
+        /// <summary>
+        /// DisplayWis control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell DisplayWis;
+
+        /// <summary>
+        /// DisplayChar control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell DisplayChar;
     }
 }
